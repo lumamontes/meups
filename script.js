e5252ca5726ab6a8fe853d5e11a0b0275743a7bd
Luma Montes <lumagoesmontes@gmail.com>
2020-08-07 22:22:43 -0300

    limpado lixo

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ botao.addEventListener("click", function(){
 document.querySelector('.close-modal').addEventListener("click", function(){
     modal.classList.remove('active')
 })
+
+
+const botaoMenu = document.querySelector('#botaoMenu')
+const menuLinks = document.querySelector('.modal-menu')
+botaoMenu.addEventListener("click", function(){
+    menuLinks.classList.add('active')
+})
+
+document.querySelector('.close-modal').addEventListener("click", function(){
+    menuLinks.classList.remove('active')
+})
